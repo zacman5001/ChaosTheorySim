09f260cbb78e2012a2da11b0477efc7d8154a33e
mathomas <thomas.ma@mail.utoronto.ca>
2017-08-22 22:11:37 -0400

    added scrolling cam

--- Assets/scripts/CharcterControllerFull.cs
+++ Assets/scripts/CharcterControllerFull.cs
@@ -2,10 +2,13 @@
 using System.Collections;
 
 public class CharcterControllerFull : MonoBehaviour {
-	
+
+    public float verticalBuffer = 1;
+    public float horizontalBuffer = 1;
 	float pitch = 0;
 	float currentPitch;
 	float pitchRange = 45;
+    public float scrollSpeed = 1f;
     public float movementSpeed = 5.0f;
 	float baseFOV;
 	float yaw;
@@ -20,15 +23,38 @@ public class CharcterControllerFull : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate () {
-		
-		//yaw = currentYaw + Input.GetAxis("Mouse X");
-		//currentYaw = yaw;
-		//transform.rotation = Quaternion.Slerp(transform.localRotation, (Quaternion.Euler(0,yaw *5,0)),  Time.deltaTime * 7);
-		//Camera.main.transform.localRotation = Quaternion.Slerp (Camera.main.transform.localRotation,  Quaternion.Euler(pitch*5, 0 , 0), Time.deltaTime * 7);
-		//pitch -= Input.GetAxis("Mouse Y");
-   
-		float forwardSpeed = Input.GetAxis("Vertical") * movementSpeed; 
-		float sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
-        transform.position = transform.position + new Vector3(sideSpeed, 0, forwardSpeed);
+
+        Vector3 velocity = new Vector3(0, 0, 0);
+
+        //yaw = currentYaw + Input.GetAxis("Mouse X");
+        //currentYaw = yaw;
+        //transform.rotation = Quaternion.Slerp(transform.localRotation, (Quaternion.Euler(0,yaw *5,0)),  Time.deltaTime * 7);
+        //Camera.main.transform.localRotation = Quaternion.Slerp (Camera.main.transform.localRotation,  Quaternion.Euler(pitch*5, 0 , 0), Time.deltaTime * 7);
+        //pitch -= Input.GetAxis("Mouse Y");
+
+        if (Input.mousePosition.x < horizontalBuffer)
+        {
+            Vector3 target = new Vector3(transform.position.x - scrollSpeed, transform.position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, 0.25F);
+        }
+        else if (Input.mousePosition.x > Screen.width - horizontalBuffer)
+        {
+            Vector3 target = new Vector3(transform.position.x + scrollSpeed, transform.position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, 0.25F);
+        }
+        else if (Input.mousePosition.y < verticalBuffer) {
+            Vector3 target = new Vector3(transform.position.x, transform.position.y, transform.position.z - scrollSpeed);
+            transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, 0.25F);
+        }
+        else if (Input.mousePosition.y > Screen.height - verticalBuffer) {
+            Vector3 target = new Vector3(transform.position.x, transform.position.y, transform.position.z + scrollSpeed);
+            transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, 0.25F);
+        }
+        else
+        {
+            float forwardSpeed = Input.GetAxis("Vertical") * movementSpeed;
+            float sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
+            transform.position = transform.position + new Vector3(sideSpeed, 0, forwardSpeed);
+        }
 	}
 }
