bf2e5d20521c799f95016fae5ce0da0e3b00222a
mathomas <thomas.ma@mail.utoronto.ca>
2017-08-24 23:52:06 -0400

    added initial variables for noble families - still need to think more about how their decisionmaking works

---
+++ Assets/scripts/localSim/NobleFamilyBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NobleFamilyBehavior : MonoBehaviour {
+
+    // Decision-making metrics
+    private int economicSatisfaction;
+    private int politicalSatisfaction;
+    private int militarySatisfaction;
+    private int powerRatio;
+
+    // Resources
+    private int food;
+    private int wood;
+    private int iron;
+    private int gold;
+    private int wealth;
+    private int militaryPower;
+
+    // Attributes
+    private int foodEfficiency;
+    private int woodEfficiency;
+    private int ironEfficiency;
+    private int goldEfficiency;
+    private int silverEfficiency;
+
+    // Holdings
+    private int population;
+    private int numHouses;
+
+    // TODO need to add code for AI just regularly maintaining itself (upkeep costs, etc.)
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
