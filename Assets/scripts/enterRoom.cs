001b911b481cd3b6b4602c22d3b7ccdea92ba1b8
zacman5001 <isaacwmorrison@gmail.com>
2017-08-30 22:48:26 -0400

    FixedLightingBugs

--- Assets/scripts/enterRoom.cs
+++ Assets/scripts/enterRoom.cs
@@ -10,19 +10,25 @@ public class enterRoom : MonoBehaviour {
 
     public void switchCamera()
     {
-        if (cameraOne.active == true)
+        if (cameraOne.activeSelf == true)
         {
             cameraOne.SetActive(false);
             lightOne.SetActive(false);
+            lightOne.GetComponent<Light>().enabled = false;
             cameraTwo.SetActive(true);
             lightTwo.SetActive(true);
+            lightTwo.GetComponent<Light>().enabled = true;
+
         }
         else
         {
             cameraOne.SetActive(true);
             lightOne.SetActive(true);
+            lightOne.GetComponent<Light>().enabled = true;
             cameraTwo.SetActive(false);
             lightTwo.SetActive(false);
+            lightTwo.GetComponent<Light>().enabled = false;
+
         }
     }
 }
