7922263a1798e5373bb3ed2ba8f8047b9aaf59ba
zacman5001 <isaacwmorrison@gmail.com>
2017-08-30 21:43:24 -0400

    Switch Room Script thing

---
+++ Assets/scripts/enterRoom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enterRoom : MonoBehaviour {
+    public GameObject lightOne;
+    public GameObject lightTwo;
+    public GameObject cameraOne;
+    public GameObject cameraTwo;
+
+    void switchCamera()
+    {
+        if (cameraOne.active == true)
+        {
+            cameraOne.SetActive(false);
+            lightOne.SetActive(false);
+            cameraTwo.SetActive(true);
+            lightTwo.SetActive(true);
+        }
+        else
+        {
+            cameraOne.SetActive(true);
+            lightOne.SetActive(true);
+            cameraTwo.SetActive(false);
+            lightTwo.SetActive(false);
+        }
+    }
+}
